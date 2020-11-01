ff10c9cee163caa893567a8b8c51aa66e3765328
Raj <rajkamal11126@gmail.com>
2020-11-01 19:56:51 +0530

    Added : Contact details in addressbook

---
+++ src/com/bridgelabz/addressbook/TestBook.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.addressbook;
+
+public class TestBook {
+	public static void main(String args[]) {
+		AddressBook ab = new AddressBook();
+		System.out.println("enter person detail");
+		ab.addPerson();
+	}
+}
